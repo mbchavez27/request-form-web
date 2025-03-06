6f7d81b2b5e7b85918d2c8a446fd52137f9fa73e
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-03-06 15:38:15 +0800

    feat: add props to request cards

--- src/app/components/requestCard/requestCard.tsx
+++ src/app/components/requestCard/requestCard.tsx
@@ -1,19 +1,24 @@
 import * as Icons from '@heroicons/react/24/outline' // Import all icons
 
-const RequestCard = () => {
+type requestType = {
+  title: string
+  desc: string
+  icon: React.ElementType
+}
+
+const RequestCard = ({ request }: { request: requestType }) => {
+  const Icon = request.icon
   return (
     <>
       <div className="flex">
         <div className="bg-[#FFFFFF] drop-shadow-lg shadow-lg px-12 py-10 rounded-md w-60 hover:opacity-60 transition duration-200 ease-in-out hover:translate-y-2">
-          <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-center items-center space-y-2">
             <div>
-              <Icons.UserGroupIcon className="w-20 h-auto"></Icons.UserGroupIcon>
+              <Icon className="w-20 h-auto"></Icon>
             </div>
             <div className="flex flex-col justify-center items-center text-[#000000] space-y-1">
-              <h1 className="font-bold text-4xl">Manpower</h1>
-              <p className="text-wrap text-md text-center">
-                Request for Allocation of Personnel
-              </p>
+              <h1 className="font-bold text-4xl">{request.title}</h1>
+              <p className="text-wrap text-md text-center">{request.desc}</p>
             </div>
           </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,34 @@
 import RequestCard from './components/requestCard/requestCard'
+import {
+  UserGroupIcon,
+  DevicePhoneMobileIcon,
+  DocumentTextIcon,
+} from '@heroicons/react/24/outline'
 
 const Home = () => {
+  const requests = [
+    {
+      title: 'Manpower',
+      desc: 'Request for Allocation of Personnel',
+      icon: UserGroupIcon,
+    },
+    {
+      title: 'Tech',
+      desc: 'Request for Tech Equipment',
+      icon: DevicePhoneMobileIcon,
+    },
+    { title: 'Permits', desc: 'Request for Permits', icon: DocumentTextIcon },
+  ]
   return (
     <>
       <div className="px-16 py-20">
         <h1 className="font-bold text-5xl">Available Requests Forms</h1>
         <div className="py-16 flex space-x-12">
-          <RequestCard></RequestCard>
-          <RequestCard></RequestCard>
-          <RequestCard></RequestCard>
+          {requests.map((request) => {
+            return (
+              <RequestCard key={request.title} request={request}></RequestCard>
+            )
+          })}
         </div>
       </div>
     </>
